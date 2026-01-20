71ea24b612d8ab689e07118f6f2f4823eade3e5d
Sourav Snigdha Mansingh <souravsnigdh@gmail.com>
2026-01-20 23:11:33 +0530

    Added few more handlers and apis

--- internal/server/server.go
+++ internal/server/server.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"time"
 
+	"github.com/Eomaxl/VideoConferenceAndLiveStreaming/internal/handlers"
+	w "github.com/Eomaxl/VideoConferenceAndLiveStreaming/pkg/webrtc"
 	"github.com/gofiber/fiber/middleware/logger"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
@@ -40,7 +42,24 @@ func Run() error {
 	app.Get("/room/:uuid/chat/websocket", websocket.New(handlers.RoomChatWebsocket))
 	app.Get("/room/:uuid/viewer/websocket", websocket.New(handlers.RoomViewerWebsocket))
 	app.Get("/stream/:ssuid", handlers.Stream)
-	app.Get("/stream/:ssuid/websocket")
-	app.Get("/stream/ssuid/chat/websocket")
-	app.Get("/stream/:ssuid/viewer/websocket")
+	app.Get("/stream/:ssuid/websocket", websocket.New(handlers.StreamWebsocket, websocket.Config{HandshakeTimeout: 10 * time.Second}))
+	app.Get("/stream/ssuid/chat/websocket", websocket.New(handlers.StreamWebsocket))
+	app.Get("/stream/:ssuid/viewer/websocket", websocket.New(handlers.StreamViewerWebsocket))
+	app.Static("/", "./assets")
+
+	w.Rooms = make(map[string]*w.Room)
+	w.Streams = make(map[string]*w.Room)
+	go dispatchKeyFrames()
+	if *cert != "" {
+		return app.ListenTLS(*addr, *cert, *key)
+	}
+	return app.Listen(*addr)
+}
+
+func dispatchKeyFrames() {
+	for range time.NewTicker(time.Second * 3).C {
+		for _, room := range w.Rooms {
+			room.Peers.DispatchKeyFrame()
+		}
+	}
 }
